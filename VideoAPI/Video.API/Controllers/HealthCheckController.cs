8b49045b120e36d000d2b0627085da78a1550b42
VenkatRangala <VenkatRangala@gmail.com>
2019-03-06 13:32:03 +0000

    HealthCheck endpoint for Video api

---
+++ VideoAPI/Video.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using VideoApi.DAL.Queries;
+using VideoApi.DAL.Queries.Core;
+using VideoApi.Domain;
+
+namespace Video.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("HealthCheck")]
+    [ApiController]
+    public class HealthCheckController : Controller
+    {
+        private readonly IQueryHandler _queryHandler;
+
+        public HealthCheckController(IQueryHandler queryHandler)
+        {
+            _queryHandler = queryHandler;
+        }
+
+        /// <summary>
+        ///     Run a health check of the service
+        /// </summary>
+        /// <returns>Error if fails, otherwise OK status</returns>
+        [HttpGet("health")]
+        [SwaggerOperation(OperationId = "CheckServiceHealth")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> Health()
+        {
+            try
+            {
+                var hearingId = Guid.NewGuid();
+                var query = new GetConferenceByIdQuery(hearingId);
+                await _queryHandler.Handle<GetConferenceByIdQuery, Conference>(query);
+            }
+            catch (Exception ex)
+            {
+                var data = new
+                {
+                    ex.Message,
+                    ex.Data
+                };
+                return StatusCode((int)HttpStatusCode.InternalServerError, data);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ VideoAPI/VideoApi.UnitTests/Controllers/HealthCheckControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Testing.Common.Helper.Builders;
+using Video.API.Controllers;
+using VideoApi.DAL.Queries;
+using VideoApi.DAL.Queries.Core;
+using VideoApi.Domain;
+
+namespace VideoApi.UnitTests.Controllers
+{
+    public class HealthCheckControllerTests
+    {
+        protected Mock<IQueryHandler> MockRepository;
+        protected HealthCheckController Controller { get; private set; }
+        [SetUp]
+        public void Setup()
+        {
+            MockRepository = new Mock<IQueryHandler>();
+        }
+
+        [Test]
+        public async Task Should_return_ok_result_when_database_is_connectedAsync()
+        {
+            var hearingId = Guid.NewGuid();
+            var conference = new ConferenceBuilder().Build();
+            var query = new GetConferenceByIdQuery(hearingId);
+
+            Controller = new HealthCheckController(MockRepository.Object);
+            MockRepository.Setup(x => x.Handle<GetConferenceByIdQuery, Conference>(query)).Returns(Task.FromResult(conference));
+
+            var result = await Controller.Health();
+            var typedResult = (OkResult)result;
+            typedResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task Should_return_internal_server_error_result_when_database_is_not_connectedAsync()
+        {
+            var hearingId = Guid.NewGuid();
+            var query = new GetConferenceByIdQuery(hearingId);
+            Controller = new HealthCheckController(null);
+            MockRepository.Setup(x => x.Handle<GetConferenceByIdQuery, Conference>(query)).Throws<Exception>();
+
+            var result = await Controller.Health();
+            var typedResult = (ObjectResult)result;
+            typedResult.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
