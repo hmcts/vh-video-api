f2b01ff17ef2539b409ca6efe333e02775a3d5d4
Shaed Parkar <shaed.parkar@hmcts.net>
2019-03-15 16:21:08 +0000

    cascade deletion when conference is deleted

---
+++ VideoAPI/VideoApi.DAL/Migrations/20190315160502_UpdateOnDeleteMaps.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoApi.DAL.Migrations
+{
+    public partial class UpdateOnDeleteMaps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ParticipantStatus_Participant_ParticipantId",
+                table: "ParticipantStatus");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ParticipantStatus_Participant_ParticipantId",
+                table: "ParticipantStatus",
+                column: "ParticipantId",
+                principalTable: "Participant",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ParticipantStatus_Participant_ParticipantId",
+                table: "ParticipantStatus");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ParticipantStatus_Participant_ParticipantId",
+                table: "ParticipantStatus",
+                column: "ParticipantId",
+                principalTable: "Participant",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
