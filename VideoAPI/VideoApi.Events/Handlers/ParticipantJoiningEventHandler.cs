2c0741e909553d8699d39da7a2ec79776f94808d
Lionel Roche <lionel.roche@hmcts.net>
2019-07-02 12:35:53 +0100

    Added Participant Joining Event handler.

---
+++ VideoAPI/VideoApi.Events/Handlers/ParticipantJoiningEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using VideoApi.DAL.Commands;
+using VideoApi.DAL.Commands.Core;
+using VideoApi.DAL.Queries.Core;
+using VideoApi.Domain.Enums;
+using VideoApi.Events.Handlers.Core;
+using VideoApi.Events.Hub;
+using VideoApi.Events.Models;
+using VideoApi.Events.ServiceBus;
+
+namespace VideoApi.Events.Handlers
+{
+    public class ParticipantJoiningEventHandler : EventHandlerBase
+    {
+        public ParticipantJoiningEventHandler(IQueryHandler queryHandler, ICommandHandler commandHandler,
+            IServiceBusQueueClient serviceBusQueueClient, IHubContext<EventHub, IEventHubClient> hubContext) : base(
+            queryHandler, commandHandler, serviceBusQueueClient, hubContext)
+        {
+        }
+
+        public override EventType EventType => EventType.Joining;
+
+        protected override async Task PublishStatusAsync(CallbackEvent callbackEvent)
+        {
+            var participantState = ParticipantState.Joining;
+
+            var command = new UpdateParticipantStatusCommand(SourceConference.Id, 
+                SourceParticipant.Id, participantState);
+            await CommandHandler.Handle(command);
+            await PublishParticipantStatusMessage(participantState);
+        }
+    }
+}
