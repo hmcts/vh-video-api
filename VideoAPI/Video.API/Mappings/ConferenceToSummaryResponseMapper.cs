a7b1b5f8b4a5847a30886e94abd8eea81ecefd6b
Valentina <Valentina.bennett@hmcts.net>
2020-01-24 09:09:34 +0000

    Add to conference response the list of active tasks

--- VideoAPI/Video.API/Mappings/ConferenceToSummaryResponseMapper.cs
+++ VideoAPI/Video.API/Mappings/ConferenceToSummaryResponseMapper.cs
@@ -12,7 +12,10 @@ namespace Video.API.Mappings
             var participantMapper = new ParticipantToSummaryResponseMapper();
             var participants = conference.GetParticipants().Select(x => participantMapper.MapParticipantToSummary(x))
                 .ToList();
-            
+
+            var taskMapper = new TaskToResponseMapper();
+            var activeTasks = conference.GetTasks().Where(x => x.Status == TaskStatus.ToDo).Select(x => taskMapper.MapTaskToResponse(x)).ToList();
+
             return new ConferenceSummaryResponse
             {
                 Id = conference.Id,
@@ -21,13 +24,14 @@ namespace Video.API.Mappings
                 CaseName = conference.CaseName,
                 ScheduledDateTime = conference.ScheduledDateTime,
                 ClosedDateTime = conference.ClosedDateTime,
-                ScheduledDuration= conference.ScheduledDuration,
+                ScheduledDuration = conference.ScheduledDuration,
                 Status = conference.GetCurrentStatus(),
                 PendingTasks = conference.GetTasks().Count(x => x.Status == TaskStatus.ToDo),
                 Participants = participants,
                 HearingRefId = conference.HearingRefId,
-                HearingVenueName = conference.HearingVenueName
+                HearingVenueName = conference.HearingVenueName,
+                Tasks = activeTasks
             };
-        }       
+        }
     }
 }
