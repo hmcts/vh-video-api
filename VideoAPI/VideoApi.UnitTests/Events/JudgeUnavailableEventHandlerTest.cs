77f1ddef73260c359a10c082a8c5f2e04c9e7f33
Valentina <Valentina.bennett@hmcts.net>
2020-02-05 08:38:09 +0000

    Added judge status unavailable

---
+++ VideoAPI/VideoApi.UnitTests/Events/JudgeUnavailableEventHandlerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using VideoApi.DAL.Commands;
+using VideoApi.Domain.Enums;
+using VideoApi.Events.Handlers;
+using VideoApi.Events.Models;
+
+
+namespace VideoApi.UnitTests.Events
+{
+    public class JudgeUnavailableEventHandlerTest : EventHandlerTestBase
+    {
+        private JudgeUnavailableEventHandler _eventHandler;
+
+        [Test]
+        public async Task should_send_unavailable_participant_messages_when_judge_unavailable()
+        {
+            _eventHandler = new JudgeUnavailableEventHandler(QueryHandlerMock.Object, CommandHandlerMock.Object,
+                ServiceBusQueueClient);
+
+            var conference = TestConference;
+
+            var participantForEvent = conference.GetParticipants().First(x => x.UserRole == UserRole.Judge);
+            var callbackEvent = new CallbackEvent
+            {
+                EventType = EventType.JudgeUnavailable,
+                EventId = Guid.NewGuid().ToString(),
+                ParticipantId = participantForEvent.Id,
+                ConferenceId = conference.Id,
+                TimeStampUtc = DateTime.UtcNow
+            };
+
+            await _eventHandler.HandleAsync(callbackEvent);
+
+            CommandHandlerMock.Verify(
+                x => x.Handle(It.Is<UpdateParticipantStatusCommand>(command =>
+                    command.ConferenceId == conference.Id &&
+                    command.ParticipantId == participantForEvent.Id &&
+                    command.ParticipantState == ParticipantState.NotSignedIn)), Times.Once);
+        }
+    }
+}
