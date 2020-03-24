892eeabb3a07f6360e7900ba597a4eb6e5b59a4f
atif.rehman <atif.rehman@outlook.com>
2020-03-24 12:54:52 +0000

    VIH-5695: Adding back the Tasks in the ConferenceSummaryResponse.cs and mapper as its used for when called through GetConferencesToday

--- VideoAPI/Video.API/Mappings/ConferenceToSummaryResponseMapper.cs
+++ VideoAPI/Video.API/Mappings/ConferenceToSummaryResponseMapper.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using VideoApi.Contract.Responses;
 using VideoApi.Domain;
+using VideoApi.Domain.Enums;
 
 namespace Video.API.Mappings
 {
@@ -12,6 +13,9 @@ namespace Video.API.Mappings
             var participants = conference.GetParticipants().Select(x => participantMapper.MapParticipantToSummary(x))
                 .ToList();
 
+            var taskMapper = new TaskToResponseMapper();
+            var activeTasks = conference.GetTasks().Where(x => x.Status == TaskStatus.ToDo).Select(x => taskMapper.MapTaskToResponse(x)).ToList();
+
             return new ConferenceSummaryResponse
             {
                 Id = conference.Id,
@@ -22,9 +26,11 @@ namespace Video.API.Mappings
                 ClosedDateTime = conference.ClosedDateTime,
                 ScheduledDuration = conference.ScheduledDuration,
                 Status = conference.GetCurrentStatus(),
+                PendingTasks = conference.GetTasks().Count(x => x.Status == TaskStatus.ToDo),
                 Participants = participants,
                 HearingRefId = conference.HearingRefId,
-                HearingVenueName = conference.HearingVenueName
+                HearingVenueName = conference.HearingVenueName,
+                Tasks = activeTasks
             };
         }
     }
--- VideoAPI/VideoApi.UnitTests/Mappings/ConferenceToSummaryMapperTests.cs
+++ VideoAPI/VideoApi.UnitTests/Mappings/ConferenceToSummaryMapperTests.cs
@@ -20,6 +20,9 @@ namespace VideoApi.UnitTests.Mappings
                 .WithConferenceStatus(ConferenceState.Closed)
                 .WithParticipant(UserRole.Judge, "Judge")
                 .WithParticipants(3)
+                .WithHearingTask("Test1")
+                .WithParticipantTask("Test2")
+                .WithJudgeTask("Test3")
                 .Build();
 
             var response = _mapper.MapConferenceToSummaryResponse(conference);
@@ -29,9 +32,46 @@ namespace VideoApi.UnitTests.Mappings
                 .Excluding(x => x.ConferenceStatuses)
                 .Excluding(x => x.State)
                 .Excluding(x => x.InstantMessageHistory)
-                .Excluding(x => x.Tasks)
             );
             response.Status.Should().BeEquivalentTo(conference.GetCurrentStatus());
+            response.PendingTasks.Should().Be(conference.GetTasks().Count(x => x.Status == TaskStatus.ToDo));
+        }
+
+        [Test]
+        public void Should_map_only_active_tasks_properties()
+        {
+            var conference = new ConferenceBuilder()
+                .WithConferenceStatus(ConferenceState.InSession)
+                .WithConferenceStatus(ConferenceState.Paused)
+                .WithConferenceStatus(ConferenceState.Closed)
+                .WithParticipant(UserRole.Judge, "Judge")
+                .WithParticipants(3)
+                .WithTask("Disconnected", TaskType.Hearing)
+                .WithTask("Task1", TaskType.Hearing)
+                .WithParticipantTask("Test2")
+                .WithJudgeTask("Test3")
+                .Build();
+
+            conference.Tasks[0].Status = TaskStatus.Done;
+
+            var response = _mapper.MapConferenceToSummaryResponse(conference);
+            response.Tasks.Count.Should().BeGreaterThan(0);
+            response.Tasks.Any(x => x.Status == TaskStatus.Done).Should().Be(false);
+        }
+
+        [Test]
+        public void Should_map_if_no_tasks_assign_to_conference()
+        {
+            var conference = new ConferenceBuilder()
+                .WithConferenceStatus(ConferenceState.InSession)
+                .WithConferenceStatus(ConferenceState.Paused)
+                .WithConferenceStatus(ConferenceState.Closed)
+                .WithParticipant(UserRole.Judge, "Judge")
+                .WithParticipants(3)
+                .Build();
+
+            var response = _mapper.MapConferenceToSummaryResponse(conference);
+            response.Tasks.Count.Should().Be(0);
         }
     }
 }
