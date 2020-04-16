1e087d02434d830c30493aa9a1010357e46bd842
Sunil DSouza <sunil.dsouza111@hmcts.net>
2020-04-16 15:56:59 +0100

    feature/VIH-5796 : Added an empty event handler to handle Help event

---
+++ VideoAPI/VideoApi.Events/Handlers/HelpEventHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VideoApi.DAL.Commands.Core;
+using VideoApi.DAL.Queries.Core;
+using VideoApi.Domain.Enums;
+using VideoApi.Events.Handlers.Core;
+using VideoApi.Events.Models;
+
+namespace VideoApi.Events.Handlers
+{
+    public class HelpEventHandler : EventHandlerBase
+    {
+        public HelpEventHandler(IQueryHandler queryHandler, ICommandHandler commandHandler) : base(
+            queryHandler, commandHandler)
+        {
+        }
+        public override EventType EventType => EventType.Help;
+
+        public override async Task HandleAsync(CallbackEvent callbackEvent)
+        {
+            
+        }
+
+        protected override async Task PublishStatusAsync(CallbackEvent callbackEvent)
+        {
+
+        }
+
+    }
+}
