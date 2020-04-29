09d656e94c2be3a2055926db4002192e60846a85
atif.rehman <atif.rehman@outlook.com>
2020-04-29 14:48:49 +0100

    feature/VIH-5916 : Add Indexes to the conference table

---
+++ VideoAPI/VideoApi.DAL/Migrations/20200429133402_CreateConferenceTableIdexes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoApi.DAL.Migrations
+{
+    public partial class CreateConferenceTableIdexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Conference_HearingRefId",
+                table: "Conference",
+                column: "HearingRefId");
+            
+            migrationBuilder.CreateIndex(
+                name: "IX_Conference_State",
+                table: "Conference",
+                column: "State");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Conference_HearingRefId",
+                table: "Conference");
+            
+            migrationBuilder.DropIndex(
+                name: "IX_Conference_State",
+                table: "Conference");
+        }
+    }
+}
