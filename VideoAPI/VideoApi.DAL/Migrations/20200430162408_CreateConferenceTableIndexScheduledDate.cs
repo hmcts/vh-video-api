d84311be6979c68dcfd1ad5407231346e4139a5e
atif.rehman <atif.rehman@outlook.com>
2020-04-30 17:27:35 +0100

    feature/VIH-5924 : Add index conference table ScheduledDateTime

---
+++ VideoAPI/VideoApi.DAL/Migrations/20200430162408_CreateConferenceTableIndexScheduledDate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoApi.DAL.Migrations
+{
+    public partial class CreateConferenceTableIndexScheduledDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Conference_ScheduledDateTime",
+                table: "Conference",
+                column: "ScheduledDateTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Conference_ScheduledDateTime",
+                table: "Conference");
+        }
+    }
+}
