27e0e0744709997b48da4204ba5eb443ad6e894b
atif.rehman <atif.rehman@outlook.com>
2020-06-08 15:33:28 +0100

    feature/VIH-5988 : Rename Judges

---
+++ VideoAPI/VideoApi.DAL/Migrations/20200608142857_RenameJudgeFirstNames.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoApi.DAL.Migrations
+{
+    public partial class RenameJudgeFirstNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Participant set Name = REPLACE(Name, 'Birmingham', 'Birmingham CFJC') FROM Participant where Name LIKE '%Birmingham Courtroom%'");
+            migrationBuilder.Sql("UPDATE Participant set Name = REPLACE(Name, 'Manchester', 'Manchester CFJC') FROM Participant where Name LIKE '%Manchester Courtroom%'");
+            migrationBuilder.Sql("UPDATE Participant set Name = REPLACE(Name, 'Taylorhouse', 'Taylor House') FROM Participant where Name LIKE '%Taylorhouse Courtroom%'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Participant set Name = REPLACE(Name, 'Birmingham CFJC', 'Birmingham') FROM Participant where Name LIKE '%Birmingham CFJC Courtroom%'");
+            migrationBuilder.Sql("UPDATE Participant set Name = REPLACE(Name, 'Manchester CFJC', 'Manchester') FROM Participant where Name LIKE '%Manchester CFJC Courtroom%'");
+            migrationBuilder.Sql("UPDATE Participant set Name = REPLACE(Name, 'Taylor House', 'Taylorhouse') FROM Participant where Name LIKE '%Taylor House Courtroom%'");
+        }
+    }
+}
