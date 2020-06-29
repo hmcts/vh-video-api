41e1370a734b4157af1447e7c3f82d17f5266eea
Valentina <Valentina.bennett@hmcts.net>
2020-06-29 17:11:16 +0100

    feature/VIH-5798 : removed UnableToJoin status, refactoring

--- VideoAPI/VideoApi.IntegrationTests/Database/Queries/GetDistinctJudgeListByFirstNameQueryTests.cs
+++ VideoAPI/VideoApi.IntegrationTests/Database/Queries/GetDistinctJudgeListByFirstNameQueryTests.cs
@@ -1,11 +1,9 @@
 using Faker;
 using FluentAssertions;
-using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Testing.Common.Helper.Builders.Domain;
 using VideoApi.DAL;
