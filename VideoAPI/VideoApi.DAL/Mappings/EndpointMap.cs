12f57674607c65bea358e86e52e002b65a420e6e
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-08-17 16:34:30 +0100

    VIH-6333 Add endpoints to conference domain

--- VideoAPI/VideoApi.DAL/Mappings/EndpointMap.cs
+++ VideoAPI/VideoApi.DAL/Mappings/EndpointMap.cs
@@ -13,6 +13,7 @@ namespace VideoApi.DAL.Mappings
             builder.HasKey(x => x.Id);
             builder.Property(x => x.DisplayName).IsRequired();
             builder.Property(x => x.SipAddress).IsRequired();
+            builder.HasIndex(x => x.SipAddress).IsUnique();
             builder.Property(x => x.Pin).IsRequired();
             builder.Property(x => x.State).IsRequired();
         }
