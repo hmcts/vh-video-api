d680206b04936f533a265b14ff18fa9a40267eed
Atif Rehman <atif.rehman@outlook.com>
2020-09-15 14:59:28 +0100

    feature/VIH-6506 : Update cloud room name, append audiostream lowercase

--- VideoAPI/Testing.Common/Configuration/CvpGetAudioFileParameters.cs
+++ VideoAPI/Testing.Common/Configuration/CvpGetAudioFileParameters.cs
@@ -2,7 +2,7 @@ namespace Testing.Common.Configuration
 {
     public class CvpGetAudioFileParameters
     {
-        public string CloudRoomName { get; set; }
+        public string CloudRoom { get; set; }
         public string Date { get; set; }
         public string CaseReference { get; set; }
     }
