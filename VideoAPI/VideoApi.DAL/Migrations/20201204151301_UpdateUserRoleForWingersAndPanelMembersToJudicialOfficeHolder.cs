e840781ecfce4c62ad6fc57bac882e4cd19f4bd8
Viren Sareen <viren.sareen@solirius.com>
2020-12-04 16:56:34 +0000

    added migration for judicial office holder

---
+++ VideoAPI/VideoApi.DAL/Migrations/20201204151301_UpdateUserRoleForWingersAndPanelMembersToJudicialOfficeHolder.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoApi.DAL.Migrations
+{
+    public partial class UpdateUserRoleForWingersAndPanelMembersToJudicialOfficeHolder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE Participant SET UserRole = 7 
+                WHERE HearingRole = 'Winger' OR HearingRole = 'Panel Member'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Method intentionally left empty
+        }
+    }
+}
