8461a69a9d77196ffb87adf413654987b45af773
Shaed Parkar <shaed.parkar@slimsolutions.company>
2021-01-08 13:22:23 +0000

    VIH-6879 Add CurrentVirtualRoom to Participant.cs

--- VideoAPI/VideoApi.DAL/Migrations/20210108123301_AddCurrentVirtualRoomToParticipant.Designer.cs
+++ VideoAPI/VideoApi.DAL/Migrations/20210108123301_AddCurrentVirtualRoomToParticipant.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VideoApi.DAL;
 
 namespace VideoApi.DAL.Migrations
 {
     [DbContext(typeof(VideoApiDbContext))]
-    partial class VideoApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210108123301_AddCurrentVirtualRoomToParticipant")]
+    partial class AddCurrentVirtualRoomToParticipant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
