53ad70be78cc44f80387d3a1f40a1a6a21332582
Levi <levi.butler@hmcts.net>
2021-02-02 14:29:53 +0000

    added new config provider

--- VideoAPI/Video.API/Program.cs
+++ VideoAPI/Video.API/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.IO;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using VH.Core.Configuration;
 
 namespace Video.API
 {
@@ -18,6 +19,7 @@ namespace Video.API
         private static IHostBuilder CreateWebHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .AddAksKeyVaultSecretProvider()
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
                     webBuilder.UseIISIntegration();
