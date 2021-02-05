729416c65d4bcb86ab6624524fc939adea36e5ba
Chris Tumolo <chris.tumolo@HMCTS.net>
2021-02-05 11:39:58 +0000

    Add usage of AddAksKeyVaultSecretProvider()

--- VideoApi/VideoApi/Program.cs
+++ VideoApi/VideoApi/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.IO;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using VH.Core.Configuration;
 
 namespace VideoApi
 {
@@ -18,11 +19,22 @@ namespace VideoApi
         // ReSharper disable once MemberCanBePrivate.Global Needed for client generation on build with nswag
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
+            const string mountPath = "/mnt/secrets/vh-video-api";
+
             return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
+                .ConfigureAppConfiguration((configBuilder) =>
+                {
+                    configBuilder.AddAksKeyVaultSecretProvider(mountPath);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
                     webBuilder.UseIISIntegration();
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(configBuilder =>
+                    {
+                        configBuilder.AddAksKeyVaultSecretProvider(mountPath);
+                    });
                 });
         }
     }
