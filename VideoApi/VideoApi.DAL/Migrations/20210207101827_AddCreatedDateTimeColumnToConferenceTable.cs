d64fe8a5e4e49ceceaad11706d7017b05b73b916
Shaed Parkar <shaed.parkar@slimsolutions.company>
2021-02-08 13:37:02 +0000

    re-add migration

---
+++ VideoApi/VideoApi.DAL/Migrations/20210207101827_AddCreatedDateTimeColumnToConferenceTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoApi.DAL.Migrations
+{
+    public partial class AddCreatedDateTimeColumnToConferenceTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDateTime",
+                table: "Conference",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "Conference");
+        }
+    }
+}
