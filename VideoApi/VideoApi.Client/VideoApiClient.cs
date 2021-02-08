83b1cf8e008839252e4f3482dae3f4402a14eec1
Shaed Parkar <shaed.parkar@slimsolutions.company>
2021-02-08 12:44:52 +0000

    Add domain enums to using statement

--- VideoApi/VideoApi.Client/VideoApiClient.cs
+++ VideoApi/VideoApi.Client/VideoApiClient.cs
@@ -7,6 +7,7 @@
 using Microsoft.AspNetCore.Mvc;
 using VideoApi.Contract.Requests;
 using VideoApi.Contract.Responses;
+using VideoApi.Domain.Enums;
 
 #pragma warning disable 108 // Disable "CS0108 '{derivedDto}.ToJson()' hides inherited member '{dtoBase}.ToJson()'. Use the new keyword if hiding was intended."
 #pragma warning disable 114 // Disable "CS0114 '{derivedDto}.RaisePropertyChanged(String)' hides inherited member 'dtoBase.RaisePropertyChanged(String)'. To make the current member override that implementation, add the override keyword. Otherwise add the new keyword."
--- VideoApi/VideoApi/Controllers/ConferenceController.cs
+++ VideoApi/VideoApi/Controllers/ConferenceController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
