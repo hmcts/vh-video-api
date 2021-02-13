42abb9dfc448b64a03501d7650292a8a2f8e47e0
Phillip Chambers <mail@phillipchambers.com>
2021-02-13 20:13:56 +0000

    lint: add namespace

--- VideoApi/VideoApi.DAL/Mappings/LinkedParticipantMap.cs
+++ VideoApi/VideoApi.DAL/Mappings/LinkedParticipantMap.cs
@@ -2,23 +2,26 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using VideoApi.Domain;
 
-public class LinkedParticipantMap : IEntityTypeConfiguration<LinkedParticipant>
+namespace VideoApi.DAL.Mappings
 {
-    public void Configure(EntityTypeBuilder<LinkedParticipant> builder)
+    public class LinkedParticipantMap : IEntityTypeConfiguration<LinkedParticipant>
     {
-        builder.ToTable(nameof(LinkedParticipant));
-        builder.HasKey(x => x.Id);
-        builder.Property(x => x.Id).IsRequired().ValueGeneratedNever();
-        builder.Property(x => x.ParticipantId).IsRequired();
-        builder.Property(x => x.LinkedId).IsRequired();
-        builder.Property(x => x.Type);
-        builder.HasOne(x => x.Participant)
-            .WithMany()
-            .HasForeignKey(x => x.ParticipantId)
-            .OnDelete(DeleteBehavior.ClientCascade);
-        builder.HasOne(x => x.Linked)
-            .WithMany()
-            .HasForeignKey(x => x.LinkedId)
-            .OnDelete(DeleteBehavior.ClientCascade);
-    }
+        public void Configure(EntityTypeBuilder<LinkedParticipant> builder)
+        {
+            builder.ToTable(nameof(LinkedParticipant));
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).IsRequired().ValueGeneratedNever();
+            builder.Property(x => x.ParticipantId).IsRequired();
+            builder.Property(x => x.LinkedId).IsRequired();
+            builder.Property(x => x.Type);
+            builder.HasOne(x => x.Participant)
+                .WithMany()
+                .HasForeignKey(x => x.ParticipantId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+            builder.HasOne(x => x.Linked)
+                .WithMany()
+                .HasForeignKey(x => x.LinkedId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }   
 }
