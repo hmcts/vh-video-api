6b830a946f11c174a8a9f0bd806627b2d1949c7a
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2021-04-15 14:39:11 +0100

    test: VIH-7597 room participants alone in consultation (#381) (#382)
    Add additional coverage for leaving logic

--- VideoApi/VideoApi.UnitTests/Services/Consultation/LeaveConsultationTests.cs
+++ VideoApi/VideoApi.UnitTests/Services/Consultation/LeaveConsultationTests.cs
@@ -17,48 +17,47 @@ namespace VideoApi.UnitTests.Services.Consultation
     {
         private AutoMock _mocker;
         private ConsultationService _sut;
-        private Conference _conference;
 
         [SetUp]
         public void Setup()
         {
             _mocker = AutoMock.GetLoose();
             _sut = _mocker.Create<ConsultationService>();
-            
-            _conference = _conference = new ConferenceBuilder()
-                .WithParticipant(UserRole.Judge, null)
-                .WithParticipant(UserRole.Individual, "Applicant")
-                .WithLinkedParticipant(UserRole.Individual, "Applicant")
-                .WithInterpreterRoom()
-                .Build();
         }
 
         [Test]
         public async Task should_transfer_linked_participant_out_of_consultation_when_last_non_linked_participant_leaves_consultation()
         {
             // arrange
-            var interpreterRoom = _conference.Rooms.OfType<ParticipantRoom>().First();
-            var participant = _conference.Participants.First(x => !x.IsJudge() && x.GetParticipantRoom() == null);
-            var consultationRoom = new ConsultationRoom(_conference.Id, "ConsultationRoom2", VirtualCourtRoomType.Participant, false);
+            var conference = new ConferenceBuilder()
+                .WithParticipant(UserRole.Judge, null)
+                .WithParticipant(UserRole.Individual, "Applicant")
+                .WithLinkedParticipant(UserRole.Individual, "Applicant")
+                .WithInterpreterRoom()
+                .Build();
+            
+            var interpreterRoom = conference.Rooms.OfType<ParticipantRoom>().First();
+            var participant = conference.Participants.First(x => !x.IsJudge() && x.GetParticipantRoom() == null);
+            var consultationRoom = new ConsultationRoom(conference.Id, "ConsultationRoom2", VirtualCourtRoomType.Participant, false);
 
-            foreach (var p in _conference.Participants.Where(x=> !x.IsJudge()))
+            foreach (var p in conference.Participants.Where(x=> !x.IsJudge()))
             {
                 consultationRoom.AddParticipant(new RoomParticipant(p.Id));
             }
             
             _mocker.Mock<IQueryHandler>().Setup(x => x.Handle<GetConferenceByIdQuery, Conference>(
-                    It.Is<GetConferenceByIdQuery>(q => q.ConferenceId == _conference.Id)))
-                .ReturnsAsync(_conference);
+                    It.Is<GetConferenceByIdQuery>(q => q.ConferenceId == conference.Id)))
+                .ReturnsAsync(conference);
             
             _mocker.Mock<IQueryHandler>().Setup(x => x.Handle<GetConsultationRoomByIdQuery, ConsultationRoom>(It.IsAny<GetConsultationRoomByIdQuery>())).ReturnsAsync(consultationRoom);
 
             // act
-            await _sut.LeaveConsultationAsync(_conference.Id, participant.Id, consultationRoom.Label,
+            await _sut.LeaveConsultationAsync(conference.Id, participant.Id, consultationRoom.Label,
                 RoomType.WaitingRoom.ToString());
 
             // assert
             _mocker.Mock<IKinlyApiClient>().Verify(x =>
-                    x.TransferParticipantAsync(_conference.Id.ToString(),
+                    x.TransferParticipantAsync(conference.Id.ToString(),
                         It.Is<TransferParticipantParams>(r =>
                             r.From == consultationRoom.Label &&
                             r.To == RoomType.WaitingRoom.ToString() &&
@@ -67,11 +66,102 @@ namespace VideoApi.UnitTests.Services.Consultation
                 , Times.Once);
             
             _mocker.Mock<IKinlyApiClient>().Verify(x =>
-                    x.TransferParticipantAsync(_conference.Id.ToString(),
+                    x.TransferParticipantAsync(conference.Id.ToString(),
                         It.Is<TransferParticipantParams>(r =>
                             r.From == consultationRoom.Label &&
                             r.To == RoomType.WaitingRoom.ToString() &&
                             r.Part_id == interpreterRoom.Id.ToString())
+                    )
+                , Times.Once);
+        }
+
+        [Test]
+        public async Task should_not_transfer_linked_participant_out_of_consultation_when_non_linked_participants_are_still_in_consultation()
+        {
+            // arrange
+            var conference = new ConferenceBuilder()
+                .WithParticipant(UserRole.Judge, null)
+                .WithParticipant(UserRole.Individual, "Applicant")
+                .WithLinkedParticipant(UserRole.Individual, "Applicant")
+                .WithParticipant(UserRole.Representative, "Respondent")
+                .WithInterpreterRoom()
+                .Build();
+            
+            var interpreterRoom = conference.Rooms.OfType<ParticipantRoom>().First();
+            var participant = conference.Participants.First(x => !x.IsJudge() && x.GetParticipantRoom() == null);
+            var consultationRoom = new ConsultationRoom(conference.Id, "ConsultationRoom2", VirtualCourtRoomType.Participant, false);
+
+            foreach (var p in conference.Participants.Where(x=> !x.IsJudge()))
+            {
+                consultationRoom.AddParticipant(new RoomParticipant(p.Id));
+            }
+            
+            _mocker.Mock<IQueryHandler>().Setup(x => x.Handle<GetConferenceByIdQuery, Conference>(
+                    It.Is<GetConferenceByIdQuery>(q => q.ConferenceId == conference.Id)))
+                .ReturnsAsync(conference);
+            
+            _mocker.Mock<IQueryHandler>().Setup(x => x.Handle<GetConsultationRoomByIdQuery, ConsultationRoom>(It.IsAny<GetConsultationRoomByIdQuery>())).ReturnsAsync(consultationRoom);
+
+            // act
+            await _sut.LeaveConsultationAsync(conference.Id, participant.Id, consultationRoom.Label,
+                RoomType.WaitingRoom.ToString());
+
+            // assert
+            _mocker.Mock<IKinlyApiClient>().Verify(x =>
+                    x.TransferParticipantAsync(conference.Id.ToString(),
+                        It.Is<TransferParticipantParams>(r =>
+                            r.From == consultationRoom.Label &&
+                            r.To == RoomType.WaitingRoom.ToString() &&
+                            r.Part_id == participant.Id.ToString())
+                    )
+                , Times.Once);
+            
+            _mocker.Mock<IKinlyApiClient>().Verify(x =>
+                    x.TransferParticipantAsync(conference.Id.ToString(),
+                        It.Is<TransferParticipantParams>(r =>
+                            r.From == consultationRoom.Label &&
+                            r.To == RoomType.WaitingRoom.ToString() &&
+                            r.Part_id == interpreterRoom.Id.ToString())
+                    )
+                , Times.Never);
+        }
+        
+        [Test]
+        public async Task should_not_transfer_participant_out_of_consultation_when_non_linked_participants_are_still_in_consultation()
+        {
+            // arrange
+            var conference = new ConferenceBuilder()
+                .WithParticipant(UserRole.Judge, null)
+                .WithParticipant(UserRole.Individual, "Applicant")
+                .WithParticipant(UserRole.Representative, "Applicant")
+                .WithParticipant(UserRole.Representative, "Respondent")
+                .Build();
+            
+            var participant = conference.Participants.First(x => !x.IsJudge() && x.GetParticipantRoom() == null);
+            var consultationRoom = new ConsultationRoom(conference.Id, "ConsultationRoom2", VirtualCourtRoomType.Participant, false);
+
+            foreach (var p in conference.Participants.Where(x=> !x.IsJudge()))
+            {
+                consultationRoom.AddParticipant(new RoomParticipant(p.Id));
+            }
+            
+            _mocker.Mock<IQueryHandler>().Setup(x => x.Handle<GetConferenceByIdQuery, Conference>(
+                    It.Is<GetConferenceByIdQuery>(q => q.ConferenceId == conference.Id)))
+                .ReturnsAsync(conference);
+            
+            _mocker.Mock<IQueryHandler>().Setup(x => x.Handle<GetConsultationRoomByIdQuery, ConsultationRoom>(It.IsAny<GetConsultationRoomByIdQuery>())).ReturnsAsync(consultationRoom);
+
+            // act
+            await _sut.LeaveConsultationAsync(conference.Id, participant.Id, consultationRoom.Label,
+                RoomType.WaitingRoom.ToString());
+
+            // assert
+            _mocker.Mock<IKinlyApiClient>().Verify(x =>
+                    x.TransferParticipantAsync(conference.Id.ToString(),
+                        It.Is<TransferParticipantParams>(r =>
+                            r.From == consultationRoom.Label &&
+                            r.To == RoomType.WaitingRoom.ToString() &&
+                            r.Part_id == participant.Id.ToString())
                     )
                 , Times.Once);
         }
