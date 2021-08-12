82469f493622034863da7f75cf19776184b498a6
alberto-hortiguela <alberto.hortiguela@hmcts.net>
2021-08-12 08:24:48 +0100

    VIH-8035 - Update UserRole EnumTypes in domain with Staff Member

--- VideoApi/VideoApi.Domain/Enums/UserRole.cs
+++ VideoApi/VideoApi.Domain/Enums/UserRole.cs
@@ -11,6 +11,7 @@ namespace VideoApi.Domain.Enums
         Representative = 6,
         JudicialOfficeHolder = 7,
         QuickLinkParticipant = 8,
-        QuickLinkObserver = 9
+        QuickLinkObserver = 9,
+        StaffMember = 10
     }
 }
