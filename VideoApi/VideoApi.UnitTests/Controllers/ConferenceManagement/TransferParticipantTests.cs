7ac740623ccd76a43b0d2c66f45424ce07596ded
Robert Bennett <robertbennett1998@outlook.com>
2021-11-05 10:38:05 +0000

    Added and fixed tests

--- VideoApi/VideoApi.UnitTests/Controllers/ConferenceManagement/TransferParticipantTests.cs
+++ VideoApi/VideoApi.UnitTests/Controllers/ConferenceManagement/TransferParticipantTests.cs
@@ -17,10 +17,11 @@ namespace VideoApi.UnitTests.Controllers.ConferenceManagement
     public class TransferParticipantTests : ConferenceManagementControllerTestBase
     {
         [Test]
-        public async Task should_move_participant_into_hearing_room()
+        public async Task should_move_participant_into_hearing_room_from_waiting_room()
         {
             var conferenceId = TestConference.Id;
             var participant = TestConference.Participants.First(x => x.UserRole == UserRole.Individual);
+            participant.CurrentConsultationRoom = null;
             var request = new TransferParticipantRequest
             {
                 ParticipantId = participant.Id,
@@ -34,12 +35,32 @@ namespace VideoApi.UnitTests.Controllers.ConferenceManagement
                 x => x.TransferParticipantAsync(conferenceId, request.ParticipantId.ToString(), RoomType.WaitingRoom.ToString(),
                     RoomType.HearingRoom.ToString()), Times.Once);
         }
-        
+
         [Test]
-        public async Task should_move_room_into_hearing_room()
+        public async Task should_move_participant_into_hearing_room_from_consultation_room()
         {
             var conferenceId = TestConference.Id;
             var participant = TestConference.Participants.First(x => x.UserRole == UserRole.Individual);
+            var request = new TransferParticipantRequest
+            {
+                ParticipantId = participant.Id,
+                TransferType = TransferType.Call
+            };
+
+            var result = await Controller.TransferParticipantAsync(conferenceId, request);
+            result.Should().BeOfType<AcceptedResult>();
+
+            VideoPlatformServiceMock.Verify(
+                x => x.TransferParticipantAsync(conferenceId, request.ParticipantId.ToString(), participant.CurrentConsultationRoom.Label,
+                    RoomType.HearingRoom.ToString()), Times.Once);
+        }
+
+        [Test]
+        public async Task should_move_room_into_hearing_room_from_waiting_room()
+        {
+            var conferenceId = TestConference.Id;
+            var participant = TestConference.Participants.First(x => x.UserRole == UserRole.Individual);
+            participant.CurrentConsultationRoom = null;
             var interpreterRoom = new ParticipantRoom(TestConference.Id, "Interpreter1", VirtualCourtRoomType.Civilian);
             interpreterRoom.SetProtectedProperty(nameof(interpreterRoom.Id), 999);
             var roomParticipant = new RoomParticipant(participant.Id)
@@ -64,7 +85,37 @@ namespace VideoApi.UnitTests.Controllers.ConferenceManagement
                 x => x.TransferParticipantAsync(conferenceId, interpreterRoom.Id.ToString(),
                     RoomType.WaitingRoom.ToString(), RoomType.HearingRoom.ToString()), Times.Once);
         }
-        
+
+        [Test]
+        public async Task should_move_room_into_hearing_room_from_consultation_room()
+        {
+            var conferenceId = TestConference.Id;
+            var participant = TestConference.Participants.First(x => x.UserRole == UserRole.Individual);
+            var interpreterRoom = new ParticipantRoom(TestConference.Id, "Interpreter1", VirtualCourtRoomType.Civilian);
+            interpreterRoom.SetProtectedProperty(nameof(interpreterRoom.Id), 999);
+            var roomParticipant = new RoomParticipant(participant.Id)
+            {
+                Room = interpreterRoom,
+                RoomId = interpreterRoom.Id
+            };
+            interpreterRoom.AddParticipant(roomParticipant);
+            participant.RoomParticipants.Add(roomParticipant);
+            UpdateConferenceQueryMock();
+
+            var request = new TransferParticipantRequest
+            {
+                ParticipantId = participant.Id,
+                TransferType = TransferType.Call
+            };
+
+            var result = await Controller.TransferParticipantAsync(conferenceId, request);
+            result.Should().BeOfType<AcceptedResult>();
+
+            VideoPlatformServiceMock.Verify(
+                x => x.TransferParticipantAsync(conferenceId, interpreterRoom.Id.ToString(),
+                    participant.CurrentConsultationRoom.Label, RoomType.HearingRoom.ToString()), Times.Once);
+        }
+
         [Test]
         public async Task should_dismiss_participant_from_hearing_room()
         {
