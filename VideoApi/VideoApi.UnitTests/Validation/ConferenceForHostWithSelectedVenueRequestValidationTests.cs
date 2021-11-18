01cc1b58f8a009c2b2ddff1e250119046ad8a786
omio-ma <84961314+omio-ma@users.noreply.github.com>
2021-11-18 10:05:41 +0000

    VIH-8438 added new endpoint to fetch todays conferences for staff member (#416)
    * added new endpoint to fetch todays conferences for staff member
    
    * added new endpoint to fetch todays conferences for staff member
    
    * removed unncessary condition to check if hearing venue is empty
    
    * updated to use utc now
    
    Co-authored-by: muktadiru <muktadiru.ahmed@google.com>

---
+++ VideoApi/VideoApi.UnitTests/Validation/ConferenceForHostWithSelectedVenueRequestValidationTests.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoApi.Contract.Requests;
+using VideoApi.Validations;
+
+namespace VideoApi.UnitTests.Validation
+{
+    public class ConferenceForHostWithSelectedVenueRequestValidationTests
+    {
+        private ConferenceForHostWithSelectedVenueRequestValidation _validator;
+
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            _validator = new ConferenceForHostWithSelectedVenueRequestValidation();
+        }
+
+        [Test]
+        public async Task fails_when_hearing_venue_is_not_specified()
+        {
+            var request = new ConferenceForStaffMembertWithSelectedVenueRequest
+            {
+                HearingVenueNames = new List<string>()
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            result.Errors.Any(x => x.ErrorMessage == ConferenceForHostWithSelectedVenueRequestValidation.HearingVenueNotSpecifiedError).Should().BeTrue();
+        }
+
+        [Test]
+        public async Task passes_validation()
+        {
+            var request = new ConferenceForStaffMembertWithSelectedVenueRequest
+            {
+                HearingVenueNames = new List<string>() { "asdsad" }
+            };
+
+            var result = await _validator.ValidateAsync(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
