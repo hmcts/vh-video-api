2f5e666a6f9ea9de213396646b802bc7aff8bf0c
Zijian Wang <Zijian.Wang@hmcts.net>
2021-12-02 16:09:36 +0000

    update unit test

--- VideoApi/VideoApi.UnitTests/Controllers/QuickLink/GetQuickLinkParticipantByIdTests.cs
+++ VideoApi/VideoApi.UnitTests/Controllers/QuickLink/GetQuickLinkParticipantByIdTests.cs
@@ -17,24 +17,29 @@ namespace VideoApi.UnitTests.Controllers.QuickLink
         public async Task Should_call_query_handler_to_quick_link_participant()
         {
             //Arrange/Act
-            await Controller.GetQuickLinkParticipantByUserName(Guid.NewGuid()+QuickLinkParticipantConst.Domain);
+            var result = await Controller.GetQuickLinkParticipantByUserName(Guid.NewGuid() + QuickLinkParticipantConst.Domain);
 
             //Assert
             QueryHandler.Verify(x => x.Handle<GetQuickLinkParticipantByIdQuery, ParticipantBase>(
                 It.IsAny<GetQuickLinkParticipantByIdQuery>()), Times.Once);
-        }
-
-        [Test]
-        public async Task Should_return_not_found_result_ifConference_is_not_found()
-        {
-           //Act
-            var result = await Controller.GetQuickLinkParticipantByUserName(Guid.NewGuid()+QuickLinkParticipantConst.Domain);
-
-            //Assert
             result.Should().NotBeNull();
             result.Should().BeAssignableTo<OkObjectResult>();
             var response = result.As<OkObjectResult>().Value.As<ParticipantSummaryResponse>();
             response.Username.Should().Equals(QuickLinksParticipant.Username);
+        }
+        
+        [Test]
+        public async Task Should_return_not_found_result_ifConference_is_not_found()
+        {
+            QueryHandler.Setup(x => x.Handle<GetQuickLinkParticipantByIdQuery, ParticipantBase>(
+                It.IsAny<GetQuickLinkParticipantByIdQuery>())).ReturnsAsync((ParticipantBase)null);
+            
+            //Act
+            var result =
+                await Controller.GetQuickLinkParticipantByUserName(Guid.NewGuid() + QuickLinkParticipantConst.Domain);
+
+            //Assert
+            result.Should().BeAssignableTo<NotFoundResult>();
         }
     }
 }
