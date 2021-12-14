2369d021db6d13d9df898c68c7b8803657f3790d
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-12-14 13:11:55 +0000

    VIH-8571 Update the response type when adding a staffmember  (#423)
    * VIH-8571 Update the response type when adding a staffmember to a conference
    
    * Added missing return type
    
    * Update the client

--- VideoApi/VideoApi.Client/VideoApiClient.cs
+++ VideoApi/VideoApi.Client/VideoApiClient.cs
@@ -702,14 +702,14 @@ namespace VideoApi.Client
         /// <param name="conferenceId">The id of the conference to add participants to</param>
         /// <param name="request">Details of the participant</param>
         /// <exception cref="VideoApiException">A server side error occurred.</exception>
-        System.Threading.Tasks.Task AddStaffMemberToConferenceAsync(System.Guid conferenceId, AddStaffMemberRequest request);
+        System.Threading.Tasks.Task<AddStaffMemberResponse> AddStaffMemberToConferenceAsync(System.Guid conferenceId, AddStaffMemberRequest request);
     
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
         /// <summary>Add staff member to a conference</summary>
         /// <param name="conferenceId">The id of the conference to add participants to</param>
         /// <param name="request">Details of the participant</param>
         /// <exception cref="VideoApiException">A server side error occurred.</exception>
-        System.Threading.Tasks.Task AddStaffMemberToConferenceAsync(System.Guid conferenceId, AddStaffMemberRequest request, System.Threading.CancellationToken cancellationToken);
+        System.Threading.Tasks.Task<AddStaffMemberResponse> AddStaffMemberToConferenceAsync(System.Guid conferenceId, AddStaffMemberRequest request, System.Threading.CancellationToken cancellationToken);
     
         /// <exception cref="VideoApiException">A server side error occurred.</exception>
         System.Threading.Tasks.Task<bool> ValidateQuickLinkAsync(System.Guid hearingId);
@@ -6036,7 +6036,7 @@ namespace VideoApi.Client
         /// <param name="conferenceId">The id of the conference to add participants to</param>
         /// <param name="request">Details of the participant</param>
         /// <exception cref="VideoApiException">A server side error occurred.</exception>
-        public System.Threading.Tasks.Task AddStaffMemberToConferenceAsync(System.Guid conferenceId, AddStaffMemberRequest request)
+        public System.Threading.Tasks.Task<AddStaffMemberResponse> AddStaffMemberToConferenceAsync(System.Guid conferenceId, AddStaffMemberRequest request)
         {
             return AddStaffMemberToConferenceAsync(conferenceId, request, System.Threading.CancellationToken.None);
         }
@@ -6046,7 +6046,7 @@ namespace VideoApi.Client
         /// <param name="conferenceId">The id of the conference to add participants to</param>
         /// <param name="request">Details of the participant</param>
         /// <exception cref="VideoApiException">A server side error occurred.</exception>
-        public async System.Threading.Tasks.Task AddStaffMemberToConferenceAsync(System.Guid conferenceId, AddStaffMemberRequest request, System.Threading.CancellationToken cancellationToken)
+        public async System.Threading.Tasks.Task<AddStaffMemberResponse> AddStaffMemberToConferenceAsync(System.Guid conferenceId, AddStaffMemberRequest request, System.Threading.CancellationToken cancellationToken)
         {
             if (conferenceId == null)
                 throw new System.ArgumentNullException("conferenceId");
@@ -6068,6 +6068,7 @@ namespace VideoApi.Client
                     content_.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json");
                     request_.Content = content_;
                     request_.Method = new System.Net.Http.HttpMethod("PUT");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
     
                     PrepareRequest(client_, request_, urlBuilder_);
     
@@ -6090,9 +6091,14 @@ namespace VideoApi.Client
                         ProcessResponse(client_, response_);
     
                         var status_ = (int)response_.StatusCode;
-                        if (status_ == 204)
+                        if (status_ == 200)
                         {
-                            return;
+                            var objectResponse_ = await ReadObjectResponseAsync<AddStaffMemberResponse>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new VideoApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            return objectResponse_.Object;
                         }
                         else
                         if (status_ == 404)
