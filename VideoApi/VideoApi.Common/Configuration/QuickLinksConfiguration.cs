8e197b1d27f4381423c561bdcc52cd1c08102bf8
FarronF <farron.flaherty@hmcts.net>
2022-01-25 13:24:30 +0000

    Updating to use RSA private key

--- VideoApi/VideoApi.Common/Configuration/QuickLinksConfiguration.cs
+++ VideoApi/VideoApi.Common/Configuration/QuickLinksConfiguration.cs
@@ -2,7 +2,8 @@ namespace VideoApi.Common.Configuration
 {
     public class QuickLinksConfiguration
     {
-        public string JwtProviderSecret { get; set; }
+        public string RsaPrivateKey { get; set; }
         public string Issuer { get; set; }
+        public string ValidAudience { get; set; }
     }
 }
