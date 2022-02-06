1ce436f7a400ee077fa9078e30fe8dd3a76ccaa3
Khizar Khan <khizarkhan.hmcts@gmail.com>
2022-02-06 17:20:12 +0000

    removed un-used using

--- VideoApi/VideoApi/Controllers/ConferenceController.cs
+++ VideoApi/VideoApi/Controllers/ConferenceController.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
