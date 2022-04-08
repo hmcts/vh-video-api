d8d5debe5cfcfcdb212b76b2145580541f11f0fc
muralikaturi <41151296+muralikaturi@users.noreply.github.com>
2022-04-08 14:18:42 +0100

    Added new query to get the necessary data to process the events

---
+++ VideoApi/VideoApi.DAL/Queries/GetConferenceByIdForEventQuery.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VideoApi.DAL.Queries.Core;
+using VideoApi.Domain;
+
+namespace VideoApi.DAL.Queries
+{
+    public class GetConferenceByIdForEventQuery : IQuery
+    {
+        public Guid ConferenceId { get; set; }
+
+        public GetConferenceByIdForEventQuery(Guid conferenceId)
+        {
+            ConferenceId = conferenceId;
+        }
+    }
+
+    public class GetConferenceByIdForEventQueryHandler : IQueryHandler<GetConferenceByIdForEventQuery, Conference>
+    {
+        private readonly VideoApiDbContext _context;
+
+        public GetConferenceByIdForEventQueryHandler(VideoApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Conference> Handle(GetConferenceByIdForEventQuery query)
+        {
+            return _context.Conferences
+                .Include(x => x.Participants)
+                .Include(x => x.Endpoints)
+                .Include(x => x.Rooms)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(x => x.Id == query.ConferenceId);
+        }
+    }
+}
