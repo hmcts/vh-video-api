066812079da099db25c19a0e263d7cabf43df48f
muralikaturi <41151296+muralikaturi@users.noreply.github.com>
2022-04-21 19:34:25 +0100

    Fix to populate the room participants as the a Participant can be in more than one room

--- VideoApi/VideoApi.DAL/Queries/GetConferenceByIdQuery.cs
+++ VideoApi/VideoApi.DAL/Queries/GetConferenceByIdQuery.cs
@@ -31,7 +31,6 @@ namespace VideoApi.DAL.Queries
             var conference = await _context.Conferences
                 .Include(x => x.Participants).ThenInclude(x => x.LinkedParticipants)
                 .Include(x => x.Endpoints)
-                .Include(x => x.Rooms)
                 .AsNoTracking()
                 .SingleOrDefaultAsync(x => x.Id == query.ConferenceId);
 
@@ -45,17 +44,22 @@ namespace VideoApi.DAL.Queries
                 .Include(x => x.RoomEndpoints)
                 .AsNoTracking().Where(x => x.ConferenceId == query.ConferenceId).ToListAsync();
 
+            conference.AddRooms(rooms);
+
             foreach (var participant in conference.Participants)
             {
                 participant.CurrentConsultationRoom = rooms.SingleOrDefault(r => r.Id == participant.CurrentConsultationRoomId) as ConsultationRoom;
-
-                var room = rooms.SingleOrDefault(r => r.RoomParticipants.Any(x => x.ParticipantId == participant.Id));
-                if (room != null)
+                
+                // A participant can be in more than one room as there is no room participant concept implemented
+                // i.e. room as a participant joining the call
+                var participantRooms = rooms.Where(r => r.RoomParticipants.Any(x => x.ParticipantId == participant.Id));
+                foreach (var room in participantRooms)
                 {
                     foreach (var roomParticipant in room.RoomParticipants.Where(x => x.ParticipantId == participant.Id))
                     {
                         roomParticipant.Room = room;
                         participant.RoomParticipants.Add(roomParticipant);
+                        roomParticipant.Participant = participant;
                     }
                 }
             }
