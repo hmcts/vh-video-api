a483359ba4e47983bcb0fc5f493652ac3580483f
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-06 10:53:36 +0000

    re-enable sonar

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -4,7 +4,7 @@ set -x
 rm -d -r ${PWD}/Coverage
 rm -d -r ${PWD}/TestResults
 
-# dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/VideoApi/Swagger/**/*,**/Program.cs,**/Startup.cs,**/Testing.Common/**/*,**/VideoApi.Common/**/*,**/VideoApi.IntegrationTests/**/*,**/VideoApi.UnitTests/**/*,**/VideoApi/Extensions/*,**/VideoApi.DAL/Migrations/**/*" /d:sonar.cpd.exclusions="**/Program.cs,**/Startup.cs,**/Testing.Common/**/*,**/VideoApi/Swagger/**/*,VideoApi/VideoApi.DAL/Migrations/*,VideoApi/VideoApi.DAL/TemplateDataForEnvironments.cs"
+dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.cs.opencover.reportsPaths="${PWD}/Coverage/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/VideoApi/Swagger/**/*,**/Program.cs,**/Startup.cs,**/Testing.Common/**/*,**/VideoApi.Common/**/*,**/VideoApi.IntegrationTests/**/*,**/VideoApi.UnitTests/**/*,**/VideoApi/Extensions/*,**/VideoApi.DAL/Migrations/**/*" /d:sonar.cpd.exclusions="**/Program.cs,**/Startup.cs,**/Testing.Common/**/*,**/VideoApi/Swagger/**/*,VideoApi/VideoApi.DAL/Migrations/*,VideoApi/VideoApi.DAL/TemplateDataForEnvironments.cs"
 
 exclusions="[Testing.Common]*,[VideoApi.Common]VideoApi.Common.*,[VideoApi.Domain]*.Ddd*,[VideoApi.DAL]*.Migrations*,[VideoApi]*.Swagger,[VideoApi.Services]*"
 
@@ -24,4 +24,4 @@ dotnet test VideoApi/VideoApi.IntegrationTests/VideoApi.IntegrationTests.csproj 
     "/p:MergeWith=${PWD}/Coverage/coverage.json" \
     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
 
-# dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
+dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
