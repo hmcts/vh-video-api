2b1a7e26cb9c30ca1249e0a93650ac0e11663d0f
william.craig <william.craig2@justice.gov.uk>
2023-03-21 14:00:10 +0000

    comments

--- VideoApi/VideoApi.Services/Contracts/IAudioPlatformService.cs
+++ VideoApi/VideoApi.Services/Contracts/IAudioPlatformService.cs
@@ -6,12 +6,7 @@ using VideoApi.Services.Responses;
 namespace VideoApi.Services.Contracts
 {
     public interface IAudioPlatformService
-    {
-        /// <summary>
-        /// Used to delete the 'old-style' wowza application per hearing model. Will eventually not be needed
-        /// once no mode hearings booked in that style exist
-        /// </summary>
-        Task<AudioPlatformServiceResponse> DeleteAudioApplicationAsync(Guid hearingId);         
+    {    
         
         /// <summary>
         /// Returns the status of the in-progress hearing recorder. Only requires that one node return a successful response
@@ -20,7 +15,7 @@ namespace VideoApi.Services.Contracts
         Task<WowzaGetStreamRecorderResponse> GetAudioStreamInfoAsync(string application, string recorder);
         
         /// <summary>
-        /// Builds the URI that Kinly will stream the audio to on the wowza node. 
+        /// Builds the URI pointing to the location the wowza application that Kinly will stream the audio to
         /// </summary>
         public string GetAudioIngestUrl(string hearingId);
         
@@ -30,10 +25,16 @@ namespace VideoApi.Services.Contracts
         public string ApplicationName { get; }
         
         /// <summary>
-        /// Use to check the status of the vms for the video-api health check. Checks status of load balancer, as
-        /// only requires one health VM to return success
+        /// Use to check the status of the wowza vms for the video-api health check. Checks status of load balancer, as
+        /// only requires one healthy VM to return success
         /// </summary>
         Task<bool> GetDiagnosticsAsync();
+
+        /// <summary>
+        /// Used to delete the 'old-style' wowza application per hearing model. Will eventually not be needed
+        /// once no mode hearings booked in that style exist
+        /// </summary>
+        Task<AudioPlatformServiceResponse> DeleteAudioApplicationAsync(Guid hearingId);     
         
         [Obsolete]
         Task<WowzaGetApplicationResponse> GetAudioApplicationInfoAsync(Guid? hearingId = null);   
