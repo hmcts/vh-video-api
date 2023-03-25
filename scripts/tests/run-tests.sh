0126540da4fa1d9ed2086b96bd35d67097883727
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-25 14:22:53 +0000

    migrate to new template

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -4,12 +4,9 @@ set -x
 rm -d -r ${PWD}/Coverage
 rm -d -r ${PWD}/TestResults
 
-dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /s:"${PWD}/vh-api-sonar-settings.xml"
-
 exclusions="[Testing.Common]*,[VideoApi.Common]VideoApi.Common.*,[VideoApi.Domain]*.Ddd*,[VideoApi.DAL]*.Migrations*,[VideoApi]*.Swagger,[VideoApi.Services]*"
 configuration=Release
 
-dotnet build VideoApi/VideoApi.sln -c $configuration
 # Script is for docker compose tests where the script is at the root level
 dotnet test VideoApi/VideoApi.UnitTests/VideoApi.UnitTests.csproj -c $configuration --no-build --results-directory ./TestResults --logger "trx;LogFileName=VideoApi-Unit-Tests-TestResults.trx" \
     "/p:CollectCoverage=true" \
@@ -24,5 +21,3 @@ dotnet test VideoApi/VideoApi.IntegrationTests/VideoApi.IntegrationTests.csproj 
     "/p:CoverletOutput=${PWD}/Coverage/" \
     "/p:MergeWith=${PWD}/Coverage/coverage.json" \
     "/p:CoverletOutputFormat=\"opencover,json,cobertura,lcov\""
-
-dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
