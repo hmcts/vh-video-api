e7231ca4ad4a9472c04cf591b2e01083709a9a42
William Craig <mr.craig1991@gmail.com>
2023-04-21 15:28:56 +0100

    Added aggregate error handling and  reorganised obsolete methods and added regions. Added descriptive comments to AudioPlatformService.cs interface. Regenerated client

--- VideoApi/VideoApi.Services/Contracts/IAudioPlatformService.cs
+++ VideoApi/VideoApi.Services/Contracts/IAudioPlatformService.cs
@@ -7,12 +7,38 @@ namespace VideoApi.Services.Contracts
 {
     public interface IAudioPlatformService
     {
-        Task<WowzaGetApplicationResponse> GetAudioApplicationInfoAsync(Guid? hearingId = null);   
-        Task<AudioPlatformServiceResponse> DeleteAudioApplicationAsync(Guid hearingId);        
-        Task<WowzaMonitorStreamResponse> GetAudioStreamMonitoringInfoAsync(Guid hearingId);        
+        /// <summary>
+        /// Returns the status of the in-progress hearing recorder. Only requires that one node return a successful response
+        /// Will throw aggregate exception if the streaming recorder can not be found on both Wowza Vms
+        /// </summary>
         Task<WowzaGetStreamRecorderResponse> GetAudioStreamInfoAsync(string application, string recorder);
+        
+        /// <summary>
+        /// Builds the URI pointing to the location the wowza application that Kinly will stream the audio to
+        /// </summary>
         public string GetAudioIngestUrl(string hearingId);
+        
+        /// <summary>
+        /// Wowza recorder application name
+        /// </summary>
         public string ApplicationName { get; }
+        
+        /// <summary>
+        /// Use to check the status of the wowza vms for the video-api health check. Checks status of load balancer, as
+        /// only requires one healthy VM to return success
+        /// </summary>
         Task<bool> GetDiagnosticsAsync();
+
+        /// <summary>
+        /// Used to delete the 'old-style' wowza application per hearing model. Will eventually not be needed
+        /// once no mode hearings booked in that style exist
+        /// </summary>
+        Task<AudioPlatformServiceResponse> DeleteAudioApplicationAsync(Guid hearingId);     
+        
+        [Obsolete("Not used in the current operations process")]
+        Task<WowzaGetApplicationResponse> GetAudioApplicationInfoAsync(Guid? hearingId = null);   
+        
+        [Obsolete("Not used in the current operations process")]  
+        Task<WowzaMonitorStreamResponse> GetAudioStreamMonitoringInfoAsync(Guid hearingId); 
     }
 }
