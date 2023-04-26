58e670ed30a8fc69ddac6a54a23aa08ba8f37968
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-04-26 11:21:33 +0100

    VIH-9841 - Update HearingDetailRequest property (#571)
    * Update HearingDetailRequest.cs
    
    * +semver: fix

--- VideoApi/VideoApi.Contract/Requests/HearingDetailRequest.cs
+++ VideoApi/VideoApi.Contract/Requests/HearingDetailRequest.cs
@@ -1,8 +1,10 @@
+using System;
+
 namespace VideoApi.Contract.Requests;
 
 public class HearingDetailRequest
 {
-    public string Time { get; set; }
+    public DateTimeOffset Time { get; set; }
     public string Judge { get; set; }
     public string CaseName { get; set; }
 }
