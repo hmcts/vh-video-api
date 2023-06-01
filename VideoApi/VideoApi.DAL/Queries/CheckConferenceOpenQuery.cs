6db68dc16788fda55ae98129c7b3b9f99783fc62
Oliver Scott <oliver.scott@justice.gov.uk>
2023-06-01 16:17:11 +0100

    VIH-9878 Use hearing ref id when checking for existing conferences (#581)
    * Use hearing ref id when checking for existing conferences
    
    * Nuget fixes
    
    * yaml fixes
    
    * More yaml updates

--- VideoApi/VideoApi.DAL/Queries/CheckConferenceOpenQuery.cs
+++ VideoApi/VideoApi.DAL/Queries/CheckConferenceOpenQuery.cs
@@ -9,16 +9,18 @@ namespace VideoApi.DAL.Queries
 {
     public class CheckConferenceOpenQuery : IQuery
     {
-        public CheckConferenceOpenQuery(DateTime scheduledDateTime, string caseNumber, string caseName)
+        public CheckConferenceOpenQuery(DateTime scheduledDateTime, string caseNumber, string caseName, Guid hearingRefId)
         {
             ScheduledDateTime = scheduledDateTime;
             CaseNumber = caseNumber;
             CaseName = caseName;
+            HearingRefId = hearingRefId;
         }
 
         public DateTime ScheduledDateTime { get; set; }
         public string CaseNumber { get; set; }
         public string CaseName { get; set; }
+        public Guid HearingRefId { get; set; }
     }
 
     public class CheckConferenceOpenQueryHandler : IQueryHandler<CheckConferenceOpenQuery, Conference>
@@ -38,6 +40,7 @@ namespace VideoApi.DAL.Queries
                 x.ScheduledDateTime == query.ScheduledDateTime
                 && x.CaseName == query.CaseName
                 && x.CaseNumber == query.CaseNumber
+                && x.HearingRefId == query.HearingRefId
                 && x.State != ConferenceState.Closed);
         }
     }
