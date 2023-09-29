fd3f94b59f40915feb741e4b382a8de6681c9d88
William Craig <mr.craig1991@gmail.com>
2023-09-29 16:00:40 +0100

    VIH-10218  Fix for UpdateParticipantDetails Command parameters overwrite hearing and case type (#607)
    * Fix for prod issue, where null parameters overwrite hearing and case role values
    
    * Tidy up

--- VideoApi/VideoApi/Controllers/ParticipantsController.cs
+++ VideoApi/VideoApi/Controllers/ParticipantsController.cs
@@ -180,10 +180,20 @@ namespace VideoApi.Controllers
                     Type = x.Type.MapToDomainEnum()
                 }).ToList();
 
-                var updateParticipantDetailsCommand = new UpdateParticipantDetailsCommand(conferenceId, participantId,
-                    request.Fullname, request.FirstName, request.LastName, request.DisplayName, request.Representee,
-                    request.ContactEmail, request.ContactTelephone, linkedParticipants,
-                    request.UserRole.MapToDomainEnum(), request.HearingRole, request.CaseTypeGroup);
+                var updateParticipantDetailsCommand = new UpdateParticipantDetailsCommand(conferenceId,
+                    participantId,
+                    request.Fullname, 
+                    request.FirstName, 
+                    request.LastName, 
+                    request.DisplayName, 
+                    request.Representee,
+                    request.ContactEmail,
+                    request.ContactTelephone, 
+                    linkedParticipants,
+                    request.UserRole.MapToDomainEnum(), 
+                    request.HearingRole,
+                    request.CaseTypeGroup);
+                
                 if (!request.Username.IsNullOrEmpty())
                 {
                     updateParticipantDetailsCommand.Username = request.Username;
