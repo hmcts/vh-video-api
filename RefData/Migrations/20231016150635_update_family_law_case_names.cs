ffc43c5166587ec2d31e85bd063ad25310887f6c
William Craig <mr.craig1991@gmail.com>
2023-10-16 16:24:57 +0100

    ref data script and migration for mrd

---
+++ RefData/Migrations/20231016150635_update_family_law_case_names.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RefData.Migrations
+{
+    public partial class update_family_law_case_names : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            SqlFileHelper.RunSqlFile("data/10181_update_family_law_case_type_names.sql", migrationBuilder);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
