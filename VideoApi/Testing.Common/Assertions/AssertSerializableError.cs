1d7dd48242aefa845313b7af704b777f91354303
Oliver Scott <oliver.scott@justice.gov.uk>
2023-11-28 11:04:05 +0000

    Remove unused code

--- VideoApi/Testing.Common/Assertions/AssertSerializableError.cs
+++ VideoApi/Testing.Common/Assertions/AssertSerializableError.cs
@@ -5,20 +5,6 @@ namespace Testing.Common.Assertions
 {
     public static class AssertSerializableError
     {
-        public static void ContainsKeyAndErrorMessage(this SerializableError error, string key, string errorMessage)
-        {
-            error.Should().NotBeNull();
-            error.ContainsKey(key).Should().BeTrue();
-            ((string[])error[key])[0].Should().Be(errorMessage);
-        }
-        
-        public static void ContainsKeyAndErrorMessage(this ValidationProblemDetails error, string key, string errorMessage)
-        {
-            error.Should().NotBeNull();
-            error.Errors.ContainsKey(key).Should().BeTrue();
-            error.Errors[key][0].Should().Be(errorMessage);
-        }
-
         public static void ContainsValidationErrors(this ObjectResult objectResult)
         {
             ((ValidationProblemDetails)objectResult.Value).Errors.Should().NotBeEmpty();
