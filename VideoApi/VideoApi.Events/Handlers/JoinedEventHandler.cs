fca02da42fa3ca6ba11fdef7a71a1f7ebeb4fc80
mk <murali.katuri@hmcts.net>
2024-04-16 21:47:37 +0100

    Transfer participant automatically when the participant joined and hearing started

--- VideoApi/VideoApi.Events/Handlers/JoinedEventHandler.cs
+++ VideoApi/VideoApi.Events/Handlers/JoinedEventHandler.cs
@@ -6,27 +6,36 @@ using VideoApi.DAL.Queries.Core;
 using VideoApi.Domain.Enums;
 using VideoApi.Events.Handlers.Core;
 using VideoApi.Events.Models;
+using VideoApi.Services.Contracts;
 
 namespace VideoApi.Events.Handlers
 {
     public class JoinedEventHandler : EventHandlerBase<JoinedEventHandler>
     {
-        public JoinedEventHandler(IQueryHandler queryHandler, ICommandHandler commandHandler, ILogger<JoinedEventHandler> logger) : base(
+        private readonly IVideoPlatformService _videoPlatformService;
+        public JoinedEventHandler(IQueryHandler queryHandler, ICommandHandler commandHandler, ILogger<JoinedEventHandler> logger, IVideoPlatformService videoPlatformService) : base(
             queryHandler, commandHandler, logger)
         {
+            _videoPlatformService = videoPlatformService;
         }
 
         public override EventType EventType => EventType.Joined;
 
         protected override Task PublishStatusAsync(CallbackEvent callbackEvent)
         {
-            var participantState =  ParticipantState.Available;
+            var participantState = ParticipantState.Available;
             var room = RoomType.WaitingRoom;
             var command = new UpdateParticipantStatusAndRoomCommand(SourceConference.Id, SourceParticipant.Id,
                 participantState, room, null);
-            
+
             _logger.LogInformation("Joined callback - {ConferenceId}/{ParticipantId}",
                 SourceConference.Id, SourceParticipant.Id);
+            if (SourceConference.State == ConferenceState.InSession &&
+                SourceParticipant.UserRole != UserRole.QuickLinkParticipant && SourceParticipant.UserRole != UserRole.QuickLinkObserver && SourceParticipant.HearingRole != "Witness")
+            {
+                _videoPlatformService.TransferParticipantAsync(SourceConference.Id, SourceParticipant.Id.ToString(),
+                  RoomType.WaitingRoom.ToString(), RoomType.HearingRoom.ToString());
+            }
             return CommandHandler.Handle(command);
         }
     }
