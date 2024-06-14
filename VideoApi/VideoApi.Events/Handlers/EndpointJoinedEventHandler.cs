60f07832a73cbb9110cb0bd848f7c168abc873b4
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-06-14 11:57:10 +0100

    VIH-10767 auto transfer late endpoint joiners

--- VideoApi/VideoApi.Events/Handlers/EndpointJoinedEventHandler.cs
+++ VideoApi/VideoApi.Events/Handlers/EndpointJoinedEventHandler.cs
@@ -6,14 +6,23 @@ using VideoApi.DAL.Queries.Core;
 using VideoApi.Domain.Enums;
 using VideoApi.Events.Handlers.Core;
 using VideoApi.Events.Models;
+using VideoApi.Services;
+using VideoApi.Services.Contracts;
 
 namespace VideoApi.Events.Handlers
 {
     public class EndpointJoinedEventHandler : EventHandlerBase<EndpointJoinedEventHandler>
     {
-        public EndpointJoinedEventHandler(IQueryHandler queryHandler, ICommandHandler commandHandler, ILogger<EndpointJoinedEventHandler> logger) : base(
+        private readonly IVideoPlatformService _videoPlatformService;
+        private readonly IFeatureToggles _featureToggles;
+
+        public EndpointJoinedEventHandler(IQueryHandler queryHandler, ICommandHandler commandHandler,
+            ILogger<EndpointJoinedEventHandler> logger, IVideoPlatformService videoPlatformService,
+            IFeatureToggles featureToggles) : base(
             queryHandler, commandHandler, logger)
         {
+            _featureToggles = featureToggles;
+            _videoPlatformService = videoPlatformService;
         }
 
         public override EventType EventType => EventType.EndpointJoined;
@@ -22,8 +31,25 @@ namespace VideoApi.Events.Handlers
         {
             const EndpointState endpointState = EndpointState.Connected;
             const RoomType room = RoomType.WaitingRoom;
-            var command = new UpdateEndpointStatusAndRoomCommand(SourceConference.Id, SourceEndpoint.Id, endpointState, room, null);
+            var command =
+                new UpdateEndpointStatusAndRoomCommand(SourceConference.Id, SourceEndpoint.Id, endpointState, room,
+                    null);
+
+            if (_featureToggles.VodafoneIntegrationEnabled())
+            {
+                TransferToHearingRoomIfHearingIsAlreadyInSession();
+            }
+
             return CommandHandler.Handle(command);
+        }
+
+        private void TransferToHearingRoomIfHearingIsAlreadyInSession()
+        {
+            if (SourceConference.State == ConferenceState.InSession)
+            {
+                _videoPlatformService.TransferParticipantAsync(SourceConference.Id, SourceEndpoint.Id.ToString(),
+                    RoomType.WaitingRoom.ToString(), RoomType.HearingRoom.ToString());
+            }
         }
     }
 }
