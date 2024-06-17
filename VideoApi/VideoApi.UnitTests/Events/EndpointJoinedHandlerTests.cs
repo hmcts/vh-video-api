bbe322b9ebe8ef6bae1ec572f1801c3b992b9980
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2024-06-17 14:37:04 +0100

    VIH-10767 auto transfer endpoints who join late (#652)

--- VideoApi/VideoApi.UnitTests/Events/EndpointJoinedHandlerTests.cs
+++ VideoApi/VideoApi.UnitTests/Events/EndpointJoinedHandlerTests.cs
@@ -6,6 +6,8 @@ using VideoApi.DAL.Commands;
 using VideoApi.Domain.Enums;
 using VideoApi.Events.Handlers;
 using VideoApi.Events.Models;
+using VideoApi.Services;
+using VideoApi.Services.Contracts;
 
 namespace VideoApi.UnitTests.Events
 {
@@ -35,6 +37,40 @@ namespace VideoApi.UnitTests.Events
                 x => x.Handle(It.Is<UpdateEndpointStatusAndRoomCommand>(command =>
                     command.ConferenceId == conference.Id && command.EndpointId == participantForEvent.Id &&
                     command.Status == EndpointState.Connected && command.Room == RoomType.WaitingRoom)), Times.Once);
+        }
+        
+        [Test]
+        public async Task
+            Should_transfer_endpoint_to_hearing_room_when_conference_is_in_session_and_feature_toggle_is_enabled()
+        {
+            var featureToggle = _mocker.Mock<IFeatureToggles>();
+            var videoPlatformServiceMock = _mocker.Mock<IVideoPlatformService>();
+            featureToggle.Setup(x => x.VodafoneIntegrationEnabled()).Returns(true);
+
+            var conference = TestConference;
+            conference.UpdateConferenceStatus(ConferenceState.InSession);
+            var endpointForEvent = conference.GetEndpoints()[0];
+            
+            var callbackEvent = new CallbackEvent
+            {
+                EventType = EventType.EndpointJoined,
+                EventId = Guid.NewGuid().ToString(),
+                ConferenceId = conference.Id,
+                ParticipantId = endpointForEvent.Id,
+                TimeStampUtc = DateTime.UtcNow
+            };
+            var updateStatusCommand = new UpdateParticipantStatusAndRoomCommand(conference.Id, endpointForEvent.Id,
+                ParticipantState.Available, RoomType.WaitingRoom, null);
+            CommandHandlerMock.Setup(x => x.Handle(updateStatusCommand));
+
+            await _sut.HandleAsync(callbackEvent);
+
+            CommandHandlerMock.Verify(
+                x => x.Handle(It.Is<UpdateEndpointStatusAndRoomCommand>(command =>
+                    command.ConferenceId == conference.Id && command.EndpointId == endpointForEvent.Id &&
+                    command.Status == EndpointState.Connected && command.Room == RoomType.WaitingRoom)), Times.Once);
+            
+            videoPlatformServiceMock.Verify(x => x.TransferParticipantAsync(conference.Id, endpointForEvent.Id.ToString(), RoomType.WaitingRoom.ToString(), RoomType.HearingRoom.ToString()), Times.Once);
         }
     }
 }
