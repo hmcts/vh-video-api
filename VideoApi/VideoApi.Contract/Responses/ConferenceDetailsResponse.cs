44c10c0fbacaf3cfad7a7e1442db11cb63949626
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-09-23 18:59:55 +0100

    Initalise list props for ConferenceDetailsResponse.cs and ConferenceCoreResponse.cs

--- VideoApi/VideoApi.Contract/Responses/ConferenceDetailsResponse.cs
+++ VideoApi/VideoApi.Contract/Responses/ConferenceDetailsResponse.cs
@@ -11,17 +11,17 @@ public class ConferenceDetailsResponse : ConferenceCoreResponse
     /// <summary>
     /// List of participants in conference
     /// </summary>
-    public new List<ParticipantResponse> Participants { get; set; }
-    
+    public new List<ParticipantResponse> Participants { get; set; } = new();
+
     /// <summary>
     /// List of connected telephone participants in conference
     /// </summary>
-    public List<TelephoneParticipantResponse> TelephoneParticipants { get; set; }
-    
+    public List<TelephoneParticipantResponse> TelephoneParticipants { get; set; } = new();
+
     /// <summary>
     /// List of endpoints in conference
     /// </summary>
-    public List<EndpointResponse> Endpoints { get; set; }
+    public List<EndpointResponse> Endpoints { get; set; } = new();
     
     /// <summary>
     /// The Kinly meeting room details
@@ -32,8 +32,8 @@ public class ConferenceDetailsResponse : ConferenceCoreResponse
     /// The option to indicate hearing audio recording
     /// </summary>
     public bool AudioRecordingRequired { get; set; }
-    
-    public List<CivilianRoomResponse> CivilianRooms { get; set; }
+
+    public List<CivilianRoomResponse> CivilianRooms { get; set; } = new();
     
     /// <summary>
     /// Ingest Url Audio Recording to be streamed to
