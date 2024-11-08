13f34d141e210c2af274adb975cc2619f7ec7773
Oliver Scott <oliver.scott@justice.gov.uk>
2024-11-08 12:41:57 +0000

    Fix query tests

--- VideoApi/VideoApi.DAL/Queries/GetInstantMessagesForConferenceQuery.cs
+++ VideoApi/VideoApi.DAL/Queries/GetInstantMessagesForConferenceQuery.cs
@@ -38,9 +38,10 @@ namespace VideoApi.DAL.Queries
 
             if(query.ParticipantName != null)
             {
+#pragma warning disable CA1862 // string.Equals is not supported in EF Core
                 instantMessages = (IOrderedQueryable<InstantMessage>) instantMessages
-                    .Where(x => x.From.Equals(query.ParticipantName, StringComparison.CurrentCultureIgnoreCase)
-                                || x.To.Equals(query.ParticipantName, StringComparison.CurrentCultureIgnoreCase));
+                    .Where(x => x.From.ToUpper() == query.ParticipantName.ToUpper() || x.To.ToUpper() == query.ParticipantName.ToUpper());
+#pragma warning restore CA1806 // string.Equals is not supported in EF Core
             }
 
             return await instantMessages.ToListAsync();
