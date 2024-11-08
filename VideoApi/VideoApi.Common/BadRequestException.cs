1a3c26a4ae5ec4bda73ae84dc025be2f4a14637b
Oliver Scott <oliver.scott@justice.gov.uk>
2024-11-08 14:02:38 +0000

    Remove obsolete attributes

--- VideoApi/VideoApi.Common/BadRequestException.cs
+++ VideoApi/VideoApi.Common/BadRequestException.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace VideoApi.Common
 {
     /// <summary>
     /// Exception to throw when input data passed downstream from the api input is in an invalid format
     /// </summary>
-    [Serializable]
     public class BadRequestException : Exception
     {
-#pragma warning disable SYSLIB0051
-        protected BadRequestException(SerializationInfo info, StreamingContext context) : base(info, context) { }
-#pragma warning restore SYSLIB0051
-
         public BadRequestException(string message) : base(message) { }
     }
 }
