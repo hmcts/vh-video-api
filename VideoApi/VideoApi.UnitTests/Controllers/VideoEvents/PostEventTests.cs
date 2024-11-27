87d6db7add8c721750b71e723132a5b8a6114b1f
William Craig <mr.craig1991@gmail.com>
2024-11-27 13:20:31 +0000

    Added test scenario for issue, and RoomType sanitation
    Signed-off-by: William Craig <mr.craig1991@gmail.com>

--- VideoApi/VideoApi.UnitTests/Controllers/VideoEvents/PostEventTests.cs
+++ VideoApi/VideoApi.UnitTests/Controllers/VideoEvents/PostEventTests.cs
@@ -1,8 +1,7 @@
+using System;
+using System.Threading.Tasks;
 using Autofac.Extras.Moq;
 using Moq;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using VideoApi.Contract.Enums;
 using VideoApi.Contract.Requests;
 using VideoApi.Controllers;
@@ -12,69 +11,117 @@ using VideoApi.Events.Handlers.Core;
 using VideoApi.Events.Models;
 using VHEventType = VideoApi.Domain.Enums.EventType;
 
-namespace VideoApi.UnitTests.Controllers.VideoEvents
+namespace VideoApi.UnitTests.Controllers.VideoEvents;
+
+public class PostEventTests
 {
-
-    public class PostEventTests
+    private AutoMock _mocker;
+    
+    private VideoEventsController _sut;
+    
+    [SetUp]
+    public void Setup()
     {
-        private AutoMock _mocker;
-
-        private VideoEventsController _sut;
-
-        [SetUp]
-        public void Setup()
+        _mocker = AutoMock.GetLoose();
+        
+        _sut = _mocker.Create<VideoEventsController>();
+        
+        _mocker.Mock<IEventHandlerFactory>().Setup(x => x.Get(It.IsAny<VHEventType>())).Returns(_mocker.Mock<IEventHandler>().Object);
+    }
+    
+    [TestCase(EventType.None, true)]
+    [TestCase(EventType.Joined, true)]
+    [TestCase(EventType.Disconnected, true)]
+    [TestCase(EventType.Transfer, true)]
+    [TestCase(EventType.Help, false)]
+    [TestCase(EventType.Start, true)]
+    [TestCase(EventType.CountdownFinished, true)]
+    [TestCase(EventType.Pause, true)]
+    [TestCase(EventType.Close, true)]
+    [TestCase(EventType.Leave, true)]
+    [TestCase(EventType.Consultation, true)]
+    [TestCase(EventType.MediaPermissionDenied, true)]
+    [TestCase(EventType.ParticipantJoining, true)]
+    [TestCase(EventType.SelfTestFailed, true)]
+    [TestCase(EventType.Suspend, true)]
+    [TestCase(EventType.VhoCall, true)]
+    [TestCase(EventType.ParticipantNotSignedIn, true)]
+    [TestCase(EventType.EndpointJoined, true)]
+    [TestCase(EventType.EndpointDisconnected, true)]
+    [TestCase(EventType.EndpointTransfer, true)]
+    [TestCase(EventType.ConnectingToEventHub, false)]
+    [TestCase(EventType.SelectingMedia, false)]
+    [TestCase(EventType.ConnectingToConference, false)]
+    [TestCase(EventType.RoomParticipantJoined, true)]
+    [TestCase(EventType.RoomParticipantDisconnected, true)]
+    [TestCase(EventType.RoomParticipantTransfer, true)]
+    [TestCase(EventType.TelephoneJoined, true)]
+    [TestCase(EventType.TelephoneTransfer, true)]
+    [TestCase(EventType.TelephoneDisconnected, true)]
+    public async Task PostEventAsync_FireEvent(EventType eventType, bool shouldHandleEvent)
+    {
+        // Arrange
+        var conferenceEventRequest = new ConferenceEventRequest
         {
-            _mocker = AutoMock.GetLoose();
-            
-            _sut = _mocker.Create<VideoEventsController>();
-        }
-
-        [TestCase(EventType.None, true)]
-        [TestCase(EventType.Joined, true)]
-        [TestCase(EventType.Disconnected, true)]
-        [TestCase(EventType.Transfer, true)]
-        [TestCase(EventType.Help, false)]
-        [TestCase(EventType.Start, true)]
-        [TestCase(EventType.CountdownFinished, true)]
-        [TestCase(EventType.Pause, true)]
-        [TestCase(EventType.Close, true)]
-        [TestCase(EventType.Leave, true)]
-        [TestCase(EventType.Consultation, true)]
-        [TestCase(EventType.MediaPermissionDenied, true)]
-        [TestCase(EventType.ParticipantJoining, true)]
-        [TestCase(EventType.SelfTestFailed, true)]
-        [TestCase(EventType.Suspend, true)]
-        [TestCase(EventType.VhoCall, true)]
-        [TestCase(EventType.ParticipantNotSignedIn, true)]
-        [TestCase(EventType.EndpointJoined, true)]
-        [TestCase(EventType.EndpointDisconnected, true)]
-        [TestCase(EventType.EndpointTransfer, true)]
-        [TestCase(EventType.ConnectingToEventHub, false)]
-        [TestCase(EventType.SelectingMedia, false)]
-        [TestCase(EventType.ConnectingToConference, false)]
-        [TestCase(EventType.RoomParticipantJoined, true)]
-        [TestCase(EventType.RoomParticipantDisconnected, true)]
-        [TestCase(EventType.RoomParticipantTransfer, true)]
-        [TestCase(EventType.TelephoneJoined, true)]
-        [TestCase(EventType.TelephoneTransfer, true)]
-        [TestCase(EventType.TelephoneDisconnected, true)]
-        public async Task PostEventAsync_FireEvent(EventType eventType, bool shouldHandleEvent)
+            EventType = eventType
+        };
+        
+        // Act
+        await _sut.PostEventAsync(conferenceEventRequest);
+        
+        // Assert
+        _mocker.Mock<ICommandHandler>().Verify(x => x.Handle(It.IsAny<SaveEventCommand>()), Times.Once);
+        _mocker.Mock<IEventHandlerFactory>().Verify(x => x.Get(It.IsAny<VHEventType>()), shouldHandleEvent ? Times.Once() : Times.Never());
+        _mocker.Mock<IEventHandler>().Verify(x => x.HandleAsync(It.IsAny<CallbackEvent>()), shouldHandleEvent ? Times.Once() : Times.Never());
+    }
+    
+    // Covers scenario highlighted in https://tools.hmcts.net/jira/browse/VIH-11170
+    [Test]
+    public async Task PostEventAsync_EdgeCase()
+    {
+        // Arrange
+        var conferenceEventRequest = new ConferenceEventRequest
         {
-            // Arrange
-            var conferenceEventRequest = new ConferenceEventRequest
-            {
-                EventType = eventType
-            };
-
-            _mocker.Mock<IEventHandlerFactory>().Setup(x => x.Get(It.IsAny<VHEventType>())).Returns(_mocker.Mock<IEventHandler>().Object);
-
-            // Act
-            await _sut.PostEventAsync(conferenceEventRequest);
-
-            // Assert
-            _mocker.Mock<ICommandHandler>().Verify(x => x.Handle(It.IsAny<SaveEventCommand>()), Times.Once);
-            _mocker.Mock<IEventHandlerFactory>().Verify(x => x.Get(It.IsAny<VHEventType>()), shouldHandleEvent ? Times.Once() : Times.Never());
-            _mocker.Mock<IEventHandler>().Verify(x => x.HandleAsync(It.IsAny<CallbackEvent>()), shouldHandleEvent ? Times.Once() : Times.Never());
-        }
+            EventId = Guid.NewGuid().ToString(),
+            EventType = EventType.RoomParticipantTransfer,
+            TimeStampUtc = DateTime.UtcNow,
+            ConferenceId = Guid.NewGuid().ToString(),
+            ParticipantId = Guid.NewGuid().ToString(),
+            ParticipantRoomId = "16036",
+            TransferFrom = "WaitingRoom",
+            TransferTo = "JudgeJOHConsultationRoom1",
+            Reason = "",
+            Phone = ""
+        };
+        
+        // Act
+        await _sut.PostEventAsync(conferenceEventRequest);
+        
+        // Assert
+        _mocker.Mock<IEventHandlerFactory>().Verify(x => x.Get(VHEventType.RoomParticipantTransfer), Times.Once);
+        
+        _mocker.Mock<ICommandHandler>().Verify(x => x.Handle(It.Is<SaveEventCommand>(cmd => 
+            cmd.ConferenceId == Guid.Parse(conferenceEventRequest.ConferenceId) &&
+            cmd.ExternalEventId == conferenceEventRequest.EventId &&
+            cmd.EventType ==  VHEventType.RoomParticipantTransfer &&
+            cmd.ExternalTimestamp == conferenceEventRequest.TimeStampUtc &&
+            cmd.TransferredFrom == (VideoApi.Domain.Enums.RoomType?)RoomType.WaitingRoom &&
+            cmd.TransferredTo == (VideoApi.Domain.Enums.RoomType?)RoomType.ConsultationRoom &&
+            cmd.Reason == conferenceEventRequest.Reason &&
+            cmd.Phone == conferenceEventRequest.Phone)), Times.Once);
+        
+        _mocker.Mock<IEventHandler>().Verify(x => x.HandleAsync(It.Is<CallbackEvent>(cbe => 
+            cbe.EventId == conferenceEventRequest.EventId && 
+            cbe.EventType == VHEventType.RoomParticipantTransfer &&
+            cbe.ConferenceId == Guid.Parse(conferenceEventRequest.ConferenceId) &&
+            cbe.Reason == conferenceEventRequest.Reason &&
+            cbe.TransferTo == VideoApi.Domain.Enums.RoomType.ConsultationRoom &&
+            cbe.TransferFrom == VideoApi.Domain.Enums.RoomType.WaitingRoom &&
+            cbe.TimeStampUtc == conferenceEventRequest.TimeStampUtc &&
+            cbe.ParticipantId == Guid.Parse(conferenceEventRequest.ParticipantId) &&
+            cbe.Phone == conferenceEventRequest.Phone &&
+            cbe.TransferredFromRoomLabel == conferenceEventRequest.TransferFrom &&
+            cbe.TransferredToRoomLabel == conferenceEventRequest.TransferTo &&
+            cbe.ParticipantRoomId == 16036)), Times.Once);
     }
 }
