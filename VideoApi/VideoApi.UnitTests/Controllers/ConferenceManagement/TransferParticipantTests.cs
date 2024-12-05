3aa1f91081fbb8624fe6dc4a582653c5a1d9aa08
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2024-12-05 16:05:08 +0000

    add tests

--- VideoApi/VideoApi.UnitTests/Controllers/ConferenceManagement/TransferParticipantTests.cs
+++ VideoApi/VideoApi.UnitTests/Controllers/ConferenceManagement/TransferParticipantTests.cs
@@ -133,6 +133,43 @@ namespace VideoApi.UnitTests.Controllers.ConferenceManagement
                 x => x.TransferParticipantAsync(conferenceId, request.ParticipantId.ToString(), RoomType.HearingRoom.ToString(),
                     RoomType.WaitingRoom.ToString()), Times.Once);
         }
+
+        [Test]
+        public async Task should_dismiss_endpoint_from_hearing_room()
+        {
+            var conferenceId = TestConference.Id;
+            var endpoint = TestConference.Endpoints[0];
+            var request = new TransferParticipantRequest
+            {
+                ParticipantId = endpoint.Id,
+                TransferType = TransferType.Dismiss
+            };
+            
+            var result = await Controller.TransferParticipantAsync(conferenceId, request);
+            result.Should().BeOfType<AcceptedResult>();
+            
+            VideoPlatformServiceMock.Verify(
+                x => x.TransferParticipantAsync(conferenceId, request.ParticipantId.ToString(), RoomType.HearingRoom.ToString(),
+                    RoomType.WaitingRoom.ToString()), Times.Once);
+        }
+
+        [Test]
+        public async Task should_return_NotFound_if_id_does_not_exist_for_endpoint_or_participant()
+        {
+            var conferenceId = TestConference.Id;
+            var request = new TransferParticipantRequest
+            {
+                ParticipantId = Guid.NewGuid(),
+                TransferType = TransferType.Dismiss
+            };
+            
+            var result = await Controller.TransferParticipantAsync(conferenceId, request);
+            result.Should().BeOfType<NotFoundObjectResult>();
+            
+            VideoPlatformServiceMock.Verify(
+                x => x.TransferParticipantAsync(conferenceId, It.IsAny<string>(), RoomType.HearingRoom.ToString(),
+                    RoomType.WaitingRoom.ToString()), Times.Never);
+        }
         
         [Test]
         public void should_return_internal_server_error_when_transfer_type_is_not_handled()
