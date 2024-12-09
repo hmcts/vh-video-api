a91a0e47a5af9b4cd2dbb867c92855c7a8bc2a10
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2024-12-09 16:03:54 +0000

    undo whitespace change

--- VideoApi/VideoApi.Events/Handlers/TransferEventHandler.cs
+++ VideoApi/VideoApi.Events/Handlers/TransferEventHandler.cs
@@ -30,8 +30,6 @@ namespace VideoApi.Events.Handlers
             
             var participantStatus = DeriveParticipantStatusForTransferEvent(callbackEvent);
 
-            
-            
             var command =
                 new UpdateParticipantStatusAndRoomCommand(SourceConference.Id, SourceParticipant.Id, participantStatus,
                     callbackEvent.TransferTo, callbackEvent.TransferredToRoomLabel);
